2a8377919f7298477a341ffaeb1969130b2bdeb7
argenaden <69447666+argenaden@users.noreply.github.com>
2021-04-17 01:58:02 +0900

    other pages

--- src/App.js
+++ src/App.js
@@ -19,44 +19,46 @@ class App extends React.Component {
             title: 'Argen Adenov',
             headerLinks: [
                 {title: 'Home', path: '/'},
-                {title: 'About', path: '/'},
-                {title: 'Contact', path: '/'}
+                {title: 'About', path: '/about'},
+                {title: 'Contact', path: '/contact'}
             ],
             home: {
                 title: 'Learning Everyday',
                 subTitle: 'Welcomes all sort of challenges',
                 text: 'Checkout my projects below'
             },
-            about: {
-                title: 'About'
-            },
             contact: {
-                title: 'Reach me out'
-            }
+                title: 'Contact me'
+            },
+            about: {
+                title: 'About me'
+            },
         }
     }
     render() {
         return (
             <Router>
-                <Container className="border-bottom" fluid={true}>
+                <Container className="p-0" fluid={true}>
+
                     <Navbar className="border-bottom" bg="transparent" expand="lg">
-                        <Navbar.Brand>Argen Adenov</Navbar.Brand>
+                        <Navbar.Brand>Gippo</Navbar.Brand>
 
                         <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
                         <Navbar.Collapse id="navbar-toggle">
-                            <Nav>
+                            <Nav className="ml-auto">
                                 <Link className="nav-link" to="/">Home</Link>
                                 <Link className="nav-link" to="/about">About</Link>
                                 <Link className="nav-link" to="/contact">Contact</Link>
-
                             </Nav>
                         </Navbar.Collapse>
                     </Navbar>
+
                     <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
-                    <Route path="/about" exact render={() => <AboutPage title={this.state.about.title} />} />
-                    <Route path="/contact" exact render={() => <ContactPage title={this.state.contact.title} />} />
+                    <Route path="/about" render={() => <ContactPage title={this.state.about.title} />} />
+                    <Route path="/contact" render={() => <AboutPage title={this.state.contact.title} />} />
 
                     <Footer />
+
                 </Container>
             </Router>
         );
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import Hero from '../components/Hero';
+import Content from '../components/Content';
 
 function AboutPage(props) {
+
     return(
-        <Hero>About page works</Hero>
+        <div>
+            <Hero title={props.title} />
+
+            <Content>
+                <p>Hello, my name is Argen. I'm a software engineer at eMoldino.</p>
+
+                <p>My dream is to one day start my own business and become an entrepreneur (currently working on some ideas).</p>
+
+                <p>I'm constantly learning new things. currently those things include gaining more experience with Vue.js, React, Java - Spring, AWS console</p>
+
+                <p>Me and my friends are working on this project. You can check it out <a href="http://45.9.190.155/" target="_blank" rel="noopener noreferrer">here</a>, or on the homepage. It is built with Java-Spring, MySQL, React.</p>
+            </Content>
+        </div>
     );
+
 }
 
 export default AboutPage;
