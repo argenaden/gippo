eb25550be05383bff5f5315097e68c41ca41e187
argenaden <69447666+argenaden@users.noreply.github.com>
2021-05-07 09:25:03 +0900

    update

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import article1 from '../assets/images/code.svg';
 import article2 from '../assets/images/travel.svg';
 import article3 from '../assets/images/coding.svg';
+import Button from "react-bootstrap/Button";
 
 function Article(props) {
 
@@ -19,6 +20,7 @@ function Article(props) {
                                 <p className="card-text">Using public APIs allows you to focus on the frontend and things that matter without worrying so much about the database and the backend.
                                     Below are 7 less-talked about public and free APIs.</p>
                             </div>
+                            <Button variant="primary">Read More</Button>
                         </div>
                     </div>
                     <div className="col-md-4">
@@ -30,6 +32,7 @@ function Article(props) {
                                 <p className="card-text">I was watching a video from JSFest 2018 where Douglas Crockford gave a presentation on the Power of the Paradigm.
                                     It is an excellent presentation if you have a spare hour to watch..</p>
                             </div>
+                            <Button variant="primary">Read More</Button>
                         </div>
                     </div>
                     <div className="col-md-4">
@@ -41,6 +44,7 @@ function Article(props) {
                                 <p className="card-text">The shorthand techniques in any programming language help you write code in a much cleaner, optimized, and shorter form.
                                     Now, let’s go through some shorthand techniques one by one..</p>
                             </div>
+                            <Button variant="primary">Read More</Button>
                         </div>
                     </div>
                 </div>
--- src/components/Podcast.js
+++ src/components/Podcast.js
@@ -5,8 +5,15 @@ function PostPage(props) {
 
     return(
         <Container fluid={true}>
+            <div className="input-group">
+                <div className="col-md-12 form-outline">
+                    <input type="search" id="form1" className="form-control" placeholder="Search"
+                           aria-label="Search"/>
+                </div>
+            </div>
             <div className="container">
                 <div className="row justify-content-start">
+
                     <div className="col-md-6">
                         <div className="media blog-media ">
                             <iframe allow="autoplay" width="130%" height="200" className="shadow-lg p-3 mb-2 bg-body rounded mx-auto"
@@ -54,6 +61,27 @@ function PostPage(props) {
                             <iframe className="shadow-lg p-3 mb-2 bg-body rounded mx-auto" allow="autoplay" width="130%" height="200"
     src="https://www.iheart.com/podcast/105-stuff-you-should-know-26940277/episode/how-the-electrical-grid-works-81169249/?embed=true"
     frameBorder="0"/>
+                        </div>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="media blog-media">
+                            <iframe className="shadow-lg p-3 mb-2 bg-body rounded mx-auto" allow="autoplay" width="130%" height="200"
+    src="https://www.iheart.com/podcast/326-the-daily-28076606/episode/a-shrinking-society-in-japan-82201898/?embed=true"
+    frameBorder="0"/>
+                        </div>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="media blog-media">
+                            <iframe className="shadow-lg p-3 mb-2 bg-body rounded mx-auto" allow="autoplay" width="130%" height="200"
+    src="https://www.iheart.com/podcast/326-the-daily-28076606/episode/why-russia-is-exporting-so-much-81640571/?embed=true"
+    frameBorder="0"/>
+                        </div>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="media blog-media">
+                            <iframe className="shadow-lg p-3 mb-2 bg-body rounded mx-auto" allow="autoplay" width="130%" height="200"
+                                    src="https://www.iheart.com/podcast/272-billionaire-boys-club-69452191/episode/introducing-billionaire-boys-club-69452192/?embed=true"
+                                    frameBorder="0"/>
                         </div>
                     </div>
                 </div>
